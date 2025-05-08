c95cc6644b121b50a50ad5daa163b2a737d0bcc3
Leonard Yu <leonard.yu@teledyne.com>
2025-05-08 23:19:46 +0200

    Add hlapi ref for mgmt.headers

--- xoa_driver/functions/headers.py
+++ xoa_driver/functions/headers.py
@@ -1,12 +1,3 @@
-################################################################
-#
-#                   HEADER BUILDER
-#
-# This script shows you how you can build your own header builder 
-# that converts human readable values into hex strings
-#
-################################################################
-
 from ipaddress import IPv4Address, IPv6Address
 from binascii import hexlify
 from xoa_driver.misc import Hex
