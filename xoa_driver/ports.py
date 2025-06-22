7167d110269e599597384642ca7e833d3a6b3835
Leonard Yu <leonard.yu@teledyne.com>
2025-06-23 01:20:27 +0200

    Remove the warning for hli_v2 import

--- xoa_driver/ports.py
+++ xoa_driver/ports.py
@@ -1,8 +1,4 @@
 #: All available test port types.
-import sys
-
-if "xoa_driver.v2" in sys.modules:
-    raise ImportError("\33[31mOnly Single interface version is allowed to being use at the same time.\33[0m")
 
 import typing
 
