4424c0fb1ca72df7429414f47daf9c10faae7b0e
Kim R. Pedersen <kim.pedersen@teledyne.com>
2026-09-09 11:25:59 +0200

    Fixed rebase bugs.

--- xoa_driver/internals/commands/p_commands.py
+++ xoa_driver/internals/commands/p_commands.py
@@ -589,8 +589,9 @@ class P_CAPABILITIES:
         * Bit 18: Can PMA Bit-mux config
         * Bit 19: Can PMA Sym-mux config
         * Bit 20: Can LLR
-        * Bit 21: Can Layer1 Event Capture
-        * Bit 22: Can Layer1 CGMII Capture
+        * Bit 21: Can PDM REACH Config
+        * Bit 22: Can Layer1 Event Capture
+        * Bit 23: Can Layer1 CGMII Capture
         """
 
         # fec_engines: int = field(XmpInt(), min_version=465)
